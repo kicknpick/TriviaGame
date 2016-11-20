1c9067d534326c27966a59e54a5e424143d66894
Travis Pickering <Pickerings@Traviss-MacBook-Pro.local>
2016-11-20 16:00:10 -0600

    changed the format of questions/answers to an array from object and added for loop

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -59,52 +59,129 @@ $(document).ready(function() {
 		};
 		
 		
-		// create object containing question/answers
-		var triviaObject = {
-			Q1: "What is the famous phrase Mr. Miyagi says in this scene?",
-			Q2: "What is the name of the special move the Karate Kid is about to deploy in this scene?",
-			Q3: "Which of the following films has the female in this picture NOT performed in?",
-			Q4: "What does Mr. Miyagi do with the chopsticks in this scene?",
-			Q5: "What is the name of the dojo represented in this picture?",
-			image1: "assets/images/cobra.png",
-			image2: "assets/images/cobra.png",
-			image3: "assets/images/cobra.png",
-			image4: "assets/images/cobra.png",
-			A1: ["Live free or die hard", "Dude, where's my car", "Wax on, wax off", "High kick, low kick"],
-			A2: ["The Happy Gilmore", "Crane Kick", "Mega Punch", "Sucker Punch"],
-			A3: ["The Saint", "Back To The Future", "Karate Kid II", "Cocktail"],
-			A4: ["Catch a fly", "Eat some great sushi", "Throw them at the Karate Kid", "Break them in half"],
-			A5:	["King Cobra", "Cobra Kai", "Ninja Turtles", "It was never mentioned during the movie"]
-		};
+		// create an array containing question, answers, correct answer and image
+		var triviaArray = [
+			{
+				question: "What is the famous phrase Mr. Miyagi says in this scene?",
+				image: "assets/images/cobra.png",
+				answer: ["Live free or die hard", "Dude, where's my car", "Wax on, wax off", "High kick, low kick"],
+				correctAnswer: "Wax on, wax off"
+			},
+			{
+				question: "What is the name of the special move the Karate Kid is about to deploy in this scene?",
+				image: "assets/images/cobra.png",
+				answer: ["The Happy Gilmore", "Crane Kick", "Mega Punch", "Sucker Punch"],
+				correctAnswer: "Crane Kick"
+			},
+			{
+				question: "Which of the following films has the female in this picture NOT performed in?",
+				image: "assets/images/cobra.png",
+				answer: ["The Saint", "Back To The Future", "Karate Kid II", "Cocktail"],
+				correctAnswer: "Karate Kid II"
+			},
+			{
+				question: "What does Mr. Miyagi do with the chopsticks in this scene?",
+				image: "assets/images/cobra.png",
+				answer: ["Catch a fly with chopsticks", "Eat some great sushi with chopsticks", "Throw the chopsticks at the Karate Kid", "Break the chopsticks in half"],
+				correctAnswer: "Catch a fly with chopsticks"
+			},
+			{
+				question: "What is the name of the dojo represented in this picture?",
+				image: "assets/images/cobra.png",
+				answer: ["King Cobra", "Cobra Kai", "Ninja Turtles", "It was never mentioned during the movie"],
+				correctAnswer: "Cobra Kai"
+			}
+		];
+		// test triviaArray
+		console.log(triviaArray[0].question);
+		console.log(triviaArray[3].answer[2]);
 		
-		// console.log(triviaObject);
+		
 
 		// function to reset the timer and question/answer buttons
 		function reset() {
 			clearInterval(count);
 			time = 31;
 			$("#answerButtons").empty();
-
-
 		};
 		
 		// ask the quest
-		function askQ1() {
-			// ask question
-			$("#question").html(triviaObject.Q1);
+		function ask() {
+			// for (i=0; i < triviaArray[i].length; i++) {
+				$("#question").html(triviaArray[0].question);
+				$("#questionImage").append(triviaArray[0].image);
 
-			$("#questionImage").append(triviaObject.image1);
-			// run for loop to display answers
-			for (i = 0; i < triviaObject.A1.length; i++ ) {
-				var btn = $("<button>" + triviaObject.A1[i] + "</button>");
-				btn.attr("answer", triviaObject.A1[i]);
-				btn.addClass("A1");
-				btn.appendTo("#answerButtons");
-			};
+				// run for loop to display answers
+				for (i = 0; i < triviaArray[0].answer[i].length; i++ ) {
+					var btn = $("<button>" + triviaArray[0].answer[i] + "</button>");
+					btn.attr("answer", triviaArray[0].answer[i]);
+					btn.addClass("A1");
+					btn.appendTo("#answerButtons");
+				};
+			// };
 			// start countdown
 			runTimer();
+			
 
-			$("#startButton").on("click", "A1" , function() {
+			
+
+
+		};
+		ask();
+
+		// function askQ2() {
+		// 	// ask question
+		// 	$("#question").html(triviaObject.Q2);
+
+		// 	$("#questionImage").append(triviaObject.image2);
+		// 	// run for loop to display answers
+		// 	for (i = 0; i < triviaObject.A2.length; i++ ) {
+		// 		var btn = $("<button>" + triviaObject.A2[i] + "</button>");
+		// 		btn.attr("answer", triviaObject.A2[i]);
+		// 		btn.addClass("A2");
+		// 		btn.appendTo("#answerButtons");
+		// 	};
+		// 	console.log(askQ2);		
+		// 	runTimer();
+		// 	// add eventListener when user chooses answer
+		// 	// $(".A1").on("click", function(){
+		// 	// 	if ($(".A1") === "Wax on, wax off")
+		// };
+			
+		// function askQ3() {
+		// 	// ask question
+		// 	$("#question").html(triviaObject.Q3);
+
+		// 	$("#questionImage").append(triviaObject.image3);
+		// 	// run for loop to display answers
+		// 	for (i = 0; i < triviaObject.A3.length; i++ ) {
+		// 		var btn = $("<button>" + triviaObject.A3[i] + "</button>");
+		// 		btn.attr("answer", triviaObject.A3[i]);
+		// 		btn.addClass("A3");
+		// 		btn.appendTo("#answerButtons");
+		// 	};
+		// 	console.log(askQ3);
+		// };		
+
+		// function askQ4() {
+		// 	// ask question
+		// 	$("#question").html(triviaObject.Q4);
+
+		// 	$("#questionImage").append(triviaObject.image4);
+		// 	// run for loop to display answers
+		// 	for (i = 0; i < triviaObject.A4.length; i++ ) {
+		// 		var btn = $("<button>" + triviaObject.A4[i] + "</button>");
+		// 		btn.attr("answer", triviaObject.A1[i]);
+		// 		btn.addClass("A4");
+		// 		btn.appendTo("#answerButtons");
+		// 	};
+		// 	console.log(askQ4);
+		// };
+
+				// $(".answer").html(A1[i]);
+		
+		
+		$("#startButton").on("click", "A1" , function() {
 				if (A1 == "Wax on, wax off") {
 					correct++;
 					("#startButton").html("That is correct, Danielson.");
@@ -120,62 +197,6 @@ $(document).ready(function() {
 					Q2();
 				}
 			});
-
-
-		};
-
-		function askQ2() {
-			// ask question
-			$("#question").html(triviaObject.Q2);
-
-			$("#questionImage").append(triviaObject.image2);
-			// run for loop to display answers
-			for (i = 0; i < triviaObject.A2.length; i++ ) {
-				var btn = $("<button>" + triviaObject.A2[i] + "</button>");
-				btn.attr("answer", triviaObject.A2[i]);
-				btn.addClass("A2");
-				btn.appendTo("#answerButtons");
-			};
-			console.log(askQ2);		
-			runTimer();
-			// add eventListener when user chooses answer
-			// $(".A1").on("click", function(){
-			// 	if ($(".A1") === "Wax on, wax off")
-		};
-			
-		function askQ3() {
-			// ask question
-			$("#question").html(triviaObject.Q3);
-
-			$("#questionImage").append(triviaObject.image3);
-			// run for loop to display answers
-			for (i = 0; i < triviaObject.A3.length; i++ ) {
-				var btn = $("<button>" + triviaObject.A3[i] + "</button>");
-				btn.attr("answer", triviaObject.A3[i]);
-				btn.addClass("A3");
-				btn.appendTo("#answerButtons");
-			};
-			console.log(askQ3);
-		};		
-
-		function askQ4() {
-			// ask question
-			$("#question").html(triviaObject.Q4);
-
-			$("#questionImage").append(triviaObject.image4);
-			// run for loop to display answers
-			for (i = 0; i < triviaObject.A4.length; i++ ) {
-				var btn = $("<button>" + triviaObject.A4[i] + "</button>");
-				btn.attr("answer", triviaObject.A1[i]);
-				btn.addClass("A4");
-				btn.appendTo("#answerButtons");
-			};
-			console.log(askQ4);
-		};
-
-				// $(".answer").html(A1[i]);
-		
-		askQ1();
 
 	});
 });
